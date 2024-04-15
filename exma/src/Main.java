0425af4ead467359cd10f5a4f0312b5296d323ec
Sergio <sergioponceplaza@gmail.com>
2024-04-15 23:45:04 +0200

    Ahora Rama

--- exma/src/Main.java
+++ exma/src/Main.java
@@ -5,4 +5,8 @@ public class Main {
             System.out.println("HOLA "+i);
         }
     }
+    //aszxsddsfsdfasdasdasdas
+    //aszxsddsfsdfasdasdasd
+    //aszxsddsfsdfasdasdasdasd
+    //sdadjasidaisdasdasdasdasd
 }
